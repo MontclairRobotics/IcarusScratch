b07525a06a79e95ebddff5ca937a4017afe54a56
Dylan Rafael <dylanrafael05@gmail.com>
2023-04-13 21:13:38 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,26 +1,76 @@
-mkdir env
+TEMP_ENV=".temp"
+TEST_F=env/.env-setup
+
+function force_copy
+{
+    echo "Forcing copy from $1 -> $2"
+    echo ''
+
+    shopt -s globstar
+
+    STARTCWD=`pwd`
+    cd $1
+
+    ISP='\n'
+
+    for FROM in **; do
+        TO=$STARTCWD/$2/$FROM
+        cp -r -f $FROM $TO
+        echo "Copying $FROM -> $2/$FROM" 
+    done
+
+    unset ISP
+
+    cd $STARTCWD
+}
+
+function announce
+{
+    clear
+    echo ',---------------------------'
+    echo "| $1"
+    echo '`---------------------------'
+    echo ''
+}
+
+if test -f $TEST_F; then
+    echo 'Project environment already set up!'
+    exit 1
+fi
+
+mkdir ./$TEMP_ENV
+mv env/* $TEMP_ENV/
 
 cd ./env
 
-echo "Installing yarn"
+announce "Installing yarn"
 npm install yarn -g
 
-echo "Cloning VM"
+announce "Cloning VM"
 git clone --depth 1 https://github.com/llk/scratch-vm.git
 
-echo "Cloning GUI"
+announce "Cloning GUI"
 git clone --depth 1 https://github.com/llk/scratch-gui.git
 
-echo "Preparing VM"
-cd scratch-vm
-yarn install
-yarn link
+# clear
+# echo "Preparing VM"
+# cd scratch-vm
+# yarn install
+# yarn link
 
-echo "Preparing GUI"
-cd ../scratch-gui
-yarn link scratch-vm
-yarn install
+# clear
+# echo "Preparing GUI"
+# cd ../scratch-gui
+# yarn link scratch-vm
+# yarn install
 
-cd ../..
+cd ..
 
-echo "Please copy the contents of this folder (except for /env) into /env, windows style"
+announce "Merging Files"
+
+force_copy $TEMP_ENV env
+rm -r $TEMP_ENV
+
+touch ./$TEST_F
+
+announce "Done!"
